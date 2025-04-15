869fec048baea5b3a97e023742abc7da9970d1e0
jaivik patel <jaivik.patel@fabaf.in>
2025-04-15 15:30:14 +0530

    mime issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ This fixes 404 and MIME issues on Render
   plugins: [react(), tailwindcss()],
-})
+});
