181008fad0ef1fd4e546ab1076b849963bd0d5e5
jaivikpatel2001 <jaivik.patel@fabaf.in>
2025-04-16 11:18:22 +0530

    api integratoin branch

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 const connectDB = async () => {
-    try {
-        const connectionString = `${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`;
-        await mongoose.connect(connectionString);
-        console.log('MongoDB Connected to LMS database...');
-    } catch (error) {
-        console.error('MongoDB Connection Failed:', error);
-        process.exit(1);
-    }
+  try {
+    const connectionString = `${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`;
+    await mongoose.connect(connectionString);
+    console.log("MongoDB Connected to LMS database...");
+  } catch (error) {
+    console.error("MongoDB Connection Failed:", error);
+    process.exit(1);
+  }
 };
-
 module.exports = connectDB;
